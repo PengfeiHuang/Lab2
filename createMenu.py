f37fbdfb597a99abed674bdba6c141bf52472aec
Pengfei Huang <huangpe@mnstate.edu>
2017-04-24 09:20:14 -0500

    created test for createMENU, added doc

--- createMenu.py
+++ createMenu.py
@@ -1,6 +1,10 @@
-# Lab2
+#Lab2
+"""
+   Create a menu base on the optionList
+"""
 def createMenu(optionList):
-    tmp = ""; ct =0
+    tmp = ""
+    ct =0
     for opt in optionList:
         tmp+= str(ct) + "." + opt+ "\n"
         ct+=1
---
+++ testMenu.py
@@ -0,0 +1,10 @@
+from createMenu import *
+
+optionList1=["aaaa","bbbb","cccc","dddd"]
+optionList2=["eeee","ffff","gggg","hhhh"]
+optionList3=["iiiii","jjjj","kkkk","lllll"]
+optionList4=["mmmmm","nnnnn","ooooo","ppppp"]
+menu1 = createMenu(optionList1)
+menu2 = createMenu(optionList2)
+menu3 = createMenu(optionList3)
+menu4 = createMenu(optionList4)
